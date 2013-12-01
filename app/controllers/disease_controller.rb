e2f66ec24c1f6e53c4a493cf6c1cbaf88984e6e3
Greg Eng <greg.eng@gmail.com>
2013-11-30 22:38:38 -0500

    update controller and view

--- app/controllers/disease_controller.rb
+++ app/controllers/disease_controller.rb
@@ -1,6 +1,10 @@
 class DiseaseController < ApplicationController
 
   def index
+    @search = Article.search do
+      fulltext params[:search]
+    end
+    @dieases = @search.results
   end
 
 end
