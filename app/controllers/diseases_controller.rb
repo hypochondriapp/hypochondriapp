2436429c7f3ff5d35a63d4596cc10cff9f7b8144
Greg Eng <greg.eng@gmail.com>
2013-11-30 23:32:35 -0500

    build highlighting for search results

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -2,7 +2,9 @@ class DiseasesController < ApplicationController
 
   def index
     @search = Disease.search do
-      fulltext params[:search]
+      fulltext params[:search] do
+        highlight :page_content
+      end
     end
     @diseases = @search.results
   end
--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,5 +1,5 @@
 class Disease < ActiveRecord::Base
   searchable do
-    text :page_content
+    text :page_content, :stored => true
   end
 end
