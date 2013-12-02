58008cd327d3de869a8ac468595a732693552fb5
emilyxxie <emilyxxie@gmail.com>
2013-12-02 14:07:52 -0500

    basic view styling...ready for Heroku

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,13 +1,19 @@
 class DiseasesController < ApplicationController
 
   def index
-    quoted_search_terms = params[:search].split(",").collect { |symptom| "\"+#{symptom.strip}\"" }.join(" ") || "sore throat, head"
-    @search = Disease.search do
-      fulltext quoted_search_terms do
-        highlight :page_content
-        minimum_match 1
+    if params[:search].nil?
+      @diseases = []
+    else
+      quoted_search_terms = params[:search].split(",").collect { |symptom| "\"+#{symptom.strip}\"" }.join(" ")
+      @search = Disease.search do
+        fulltext quoted_search_terms do
+          highlight :page_content
+          minimum_match 1
+        end
       end
+      @diseases = @search.results
     end
-    @diseases = @search.results
   end
+
+
 end
