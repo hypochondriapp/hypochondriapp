0957133f4abb443b5bcfc5d2042fc7bc836b9a76
Greg Eng <greg.eng@gmail.com>
2013-12-02 11:25:20 -0500

    comma separated search terms

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,10 +1,11 @@
 class DiseasesController < ApplicationController
 
   def index
+    quoted_search_terms = params[:search].split(",").collect { |symptom| "\"+#{symptom.strip}\"" }.join(" ")
+    # raise
     @search = Disease.search do
-      fulltext params[:search] do
+      fulltext quoted_search_terms do
         highlight :page_content
-        minimum_match 1
       end
     end
     @diseases = @search.results
