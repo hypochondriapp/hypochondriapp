fff88decc01f9c2cab2a4c2ebb698dc1cd48fe41
David Bella <dbella1@gmail.com>
2013-12-03 10:32:58 -0500

    Added ajaxified button to make search results appear jqueriefied

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -4,15 +4,28 @@ class DiseasesController < ApplicationController
     if params[:search].nil?
       @diseases = []
     else
-      quoted_search_terms = params[:search].split(",").collect { |symptom| "\"+#{symptom.strip}\"" }.join(" ")
+      quoted_search_terms = params[:search].split(",").collect do |symptom|
+        "\"+#{symptom.strip}\"" 
+      end.join(" ")
+
       @search = Disease.search do
         fulltext quoted_search_terms do
           highlight :page_content
           minimum_match 1
         end
       end
+
       @diseases = @search.results
+
+      @disease_name = @diseases[0..2].collect(&:name).max_by(&:length)
+      @disease_url = Disease.find_by(:name => @disease_name).try(:url)
+
+      respond_to do |format|
+        format.html { render action: "index" }
+        format.js
+      end
     end
+
   end
 
 
